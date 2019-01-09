e1c88f48035a3b7ffecff5bf3041fdd27d811b68
Liliya Zavatskaya <zavatskaya.liliya@icloud.com>
2019-01-09 09:07:48 +0600

    Initial Commit

---
+++ UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit
+//
+//  Created by Liliya Zavatskaya on 1/9/19.
+//  Copyright © 2019 Liliya Zavatskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
