87c4b4407f9ee6d0d5c4e0c7e31e4081e3f6dbc4
Liliya Zavatskaya <liliyazavatskaya@MacBook-Pro-Liliya.local>
2019-01-09 09:11:22 +0600

    ButtonLabel - 1

--- UIKit/ViewController.swift
+++ UIKit/ViewController.swift
@@ -1,20 +1,44 @@
-//
-//  ViewController.swift
-//  UIKit
-//
-//  Created by Liliya Zavatskaya on 1/9/19.
-//  Copyright © 2019 Liliya Zavatskaya. All rights reserved.
-//
+// ButtonLabel - 1
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        label.isHidden = true
+        label.font = label.font.withSize(35)
+        label.textColor = .red
+        
+        button.setTitle("Get result", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .green
+        
+        
     }
-
-
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        
+        if label.isHidden {
+            
+            label.isHidden = false
+            label.text = "Hello, world!"
+            button.setTitle("Clear", for: .normal)
+            button.setTitleColor(.white, for: .normal)
+            button.backgroundColor = .red
+            
+        } else {
+            
+            label.isHidden = true
+            button.setTitle("Get result", for: .normal)
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .green
+            
+        }
+    }
+    
 }
-
